7a2de8a03d0096735d93752ffdd12b5b835d4d03
mabans <kamauian614@gmail.com>
2024-01-28 07:15:38 +0300

    learnt pandas

---
+++ day-26/main.py
@@ -0,0 +1,17 @@
+numbers = [1, 1, 3, 5,8,12, 13, 21, 34 ,55]
+square_numbers = [square*square for square in numbers]
+print(square_numbers)
+
+list_of_strings = input().split(',')
+numbers = [int(x) for x in list_of_strings]
+
+result = [num for num in numbers if num%2==0]
+
+print(result)
+
+
+sentence = input()
+result = {word: print(word) for word in sentence.split()}
+
+print(result)
+
