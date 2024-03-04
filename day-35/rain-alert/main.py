43a90eaaf291e71b61721d5ce2f3a44cd7f9f72c
Ian <60256417+IanMabans@users.noreply.github.com>
2024-03-04 13:52:59 +0300

    Update main.py

--- day-35/rain-alert/main.py
+++ day-35/rain-alert/main.py
@@ -6,7 +6,6 @@ from twilio.rest import Client
 account_sid = os.environ.get('account_sid')
 auth_token = os.environ.get('auth_token')
 
-
 owm_endpoints = 'https://api.openweathermap.org/data/2.5/forecast'
 
 api_key = os.environ.get('api_key')
@@ -16,17 +15,18 @@ weather_params = {
     'appid': api_key,
     'cnt': 4,
 }
-# I am using Kiambu kenya coordinates
 
 response = requests.get(owm_endpoints, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-# print(weather_data["list"][0]['weather'][0])
+
 will_rain = False
 for hour_data in weather_data["list"]:
     condition_code = hour_data["weather"][0]['id']
-    if int(condition_code) < 700:
+    # Check if the condition code indicates rain
+    if 200 <= int(condition_code) <= 531:
         will_rain = True
+        break  # No need to continue checking if we've found rain
 
 if will_rain:
     client = Client(account_sid, auth_token)
@@ -34,7 +34,9 @@ if will_rain:
     message = client.messages.create(
         from_='[api sender number]',
         to='[your phone number]',
-        body='Its going to rain Ian, carry an umbrella ☔'
+        body='It\'s going to rain Ian, carry an umbrella ☔'
     )
 
     print(message.status)
+else:
+    print("No rain expected.")
